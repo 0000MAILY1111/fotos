5d245d3a8aa9fe940771842789d9c2b3738e1bc7
0000MAILY1111 <valdezmelizza19@gmail.com>
2024-09-30 21:57:53 -0300

    arreglo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-import 'bootstrap/dist/css/bootstrap.min.css';   ///si algo no carga es esto
-import 'bootstrap/dist/js/bootstrap.bundle.min';  ///si algo no carga es esto
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
